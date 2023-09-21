664c88ea706cdfdb231a536eb1dbf03fde7e5306
mchez0k <mchezok@gmail.com>
2023-09-21 22:13:29 +0300

    Добавил решение дз 1, вроде рабочее

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace TPiB
+﻿using Microsoft.VisualBasic;
+
+namespace TPiB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите через пробел расстояние, угол дельта, угол альфа:");
+            string[] data = Console.ReadLine().Split();
+            double rad = double.Parse(data[0]);
+            double delta = double.Parse(data[1]);
+            double alpha = double.Parse(data[2]);
+            Solution(rad, delta, alpha);
         }
+
+        static void Solution(double rad, double delta, double alpha)
+        {
+            double x, y, z;
+            x = rad * Math.Cos(ToRad(delta)) * Math.Cos(ToRad(alpha));
+            y = rad * Math.Cos(ToRad(delta)) * Math.Sin(ToRad(alpha));
+            z = rad * Math.Sin(ToRad(delta));
+            Console.WriteLine($"X = {x} \nY = {y}\n Z = {z}");
+        }
+
+        static double ToRad(double angle) => (Math.PI * angle / 180.0);
     }
 }
