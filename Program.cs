051be6326ad76cd3e92a3582e83c05232d5c7abd
mchez0k <mchezok@gmail.com>
2023-09-21 22:55:12 +0300

    Добавил второе задание на ГОСК

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualBasic;
+﻿using System;
 
 namespace TPiB
 {
@@ -11,16 +11,30 @@ namespace TPiB
             double rad = double.Parse(data[0]);
             double delta = double.Parse(data[1]);
             double alpha = double.Parse(data[2]);
-            Solution(rad, delta, alpha);
+            double x, y, z;
+            GetAGESK(rad, delta, alpha, out x, out y, out z);
+            Console.WriteLine("Введите момент S:");
+            double s = double.Parse(Console.ReadLine());
+            GetGOSK(x, y, z, s);
+
         }
 
-        static void Solution(double rad, double delta, double alpha)
+        static void GetAGESK(double rad, double delta, double alpha, out double X, out double Y, out double Z)
         {
-            double x, y, z;
-            x = rad * Math.Cos(ToRad(delta)) * Math.Cos(ToRad(alpha));
-            y = rad * Math.Cos(ToRad(delta)) * Math.Sin(ToRad(alpha));
-            z = rad * Math.Sin(ToRad(delta));
-            Console.WriteLine($"X = {x} \nY = {y}\n Z = {z}");
+            X = rad * Math.Cos(ToRad(delta)) * Math.Cos(ToRad(alpha));
+            Y = rad * Math.Cos(ToRad(delta)) * Math.Sin(ToRad(alpha));
+            Z = rad * Math.Sin(ToRad(delta));
+            Console.WriteLine($"Координаты в АГЭСК:\nX = {X} \nY = {Y}\nZ = {Z}");
+        }
+
+        static void GetGOSK(double x, double y, double z, double s) 
+        {
+            double Xg, Yg, Zg;
+            Console.WriteLine($"Координаты в ГОСК для момента S = 0:\nX = {x} \nY = {y}\nZ = {z}");
+            Xg = x * Math.Cos(ToRad(s)) + y * Math.Sin(ToRad(s));
+            Yg = -x * Math.Sin(ToRad(s)) + y * Math.Cos(ToRad(s));
+            Zg = z;            
+            Console.WriteLine($"Координаты в ГОСК для момента S = {s}:\nX = {Xg} \nY = {Yg}\nZ = {Zg}");
         }
 
         static double ToRad(double angle) => (Math.PI * angle / 180.0);
